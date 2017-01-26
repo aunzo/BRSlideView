6238c1380dd2cf060d6a2af30394408cdaa4678b
Patarapon Tokham <patarapon.t@gmail.com>
2017-01-26 16:45:52 +0700

    Finish Release Version

---
+++ BRSlideView/BRSlideView.h
@@ -0,0 +1,19 @@
+//
+//  BRSlideView.h
+//  BRSlideView
+//
+//  Created by Blackraito on 1/26/2560 BE.
+//  Copyright © 2560 Patarapon Tokham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BRSlideView.
+FOUNDATION_EXPORT double BRSlideViewVersionNumber;
+
+//! Project version string for BRSlideView.
+FOUNDATION_EXPORT const unsigned char BRSlideViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BRSlideView/PublicHeader.h>
+
+
